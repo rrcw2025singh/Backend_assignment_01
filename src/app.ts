87390a5bbd4bc27b67533ed6d679ea12259506cc
karanvir singh <ksingh34@academic.rrc.ca>
2026-01-21 21:45:56 -0600

    Added the test scripts.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express, { Express } from "express";
+import healthRoutes from "./api/v1/routes/healthRoutes";
+
 // Initializing the express application
 const app: Express = express();
  
 // Parsing JSON
 app.use(express.json());
- 
+
+// Routes
+app.use("/api/v1", healthRoutes);
+
 // Defining the route
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+    res.send("Back-end API is running");
 });
  
  
