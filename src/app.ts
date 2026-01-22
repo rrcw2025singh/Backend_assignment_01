465a96aa2e225b726ee72695857d319d9bad0ec5
karanvir singh <ksingh34@academic.rrc.ca>
2026-01-22 16:02:42 -0600

    Added the portfolio performance endpoints.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import healthRoutes from "./api/v1/routes/healthRoutes";
+import portfolioRoutes from "./api/v1/routes/portfolioRoutes";
 
 // Initializing the express application
 const app: Express = express();
@@ -9,6 +10,9 @@ app.use(express.json());
 
 // Routes
 app.use("/api/v1", healthRoutes);
+
+// Portfolio Routes
+app.use("/api/v1", portfolioRoutes);
 
 // Defining the route
 app.get("/", (req, res) => {
